97cb6554613b22e48b6d23b24de22e37d257cf4e
Snowy <snowy@snowymouse.com>
2022-03-30 15:33:18 -0500

    Generate the windows.rc on compile

--- build.rs
+++ build.rs
@@ -1,11 +1,55 @@
 extern crate embed_resource;
 
+fn var(what : &str) -> String {
+    std::env::var(what).unwrap()
+}
+
+use std::fs::File;
+use std::path::Path;
+use std::io::Write;
+
 fn main() {
+    let gorilla_version = var("CARGO_PKG_VERSION");
+    let gorilla_version_dot = gorilla_version.replace(".",",");
+    let gorilla_pkg = var("CARGO_PKG_NAME");
+
     // Compile in windows.rc if Windows
-    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
-        embed_resource::compile("windows.rc");
+    if var("CARGO_CFG_TARGET_OS") == "windows" {
+        let windows_rc_path_raw = format!("{}/windows.rc", var("OUT_DIR"));
+        let windows_rc_path = Path::new(&windows_rc_path_raw);
+
+        write!(File::create(windows_rc_path).unwrap(),
+"1 VERSIONINFO
+FILEVERSION      {gorilla_version_dot},0
+PRODUCTVERSION   {gorilla_version_dot},0
+BEGIN
+    BLOCK \"StringFileInfo\"
+    BEGIN
+        BLOCK \"040904B0\"
+        BEGIN
+            VALUE \"Comments\",         \"Definition dumper for guerilla.exe\"
+            VALUE \"CompanyName\",      \"Snowy Mouse\"
+            VALUE \"FileDescription\",  \"Definition dumper for guerilla.exe\"
+            VALUE \"FileVersion\",      \"{gorilla_version}\"
+            VALUE \"InternalName\",     \"{gorilla_pkg}.exe\"
+            VALUE \"OriginalFilename\", \"{gorilla_pkg}.exe\"
+            VALUE \"ProductName\",      \"{gorilla_pkg}.exe\"
+            VALUE \"ProductVersion\",   \"{gorilla_version}\"
+            VALUE \"LegalCopyright\",   \"'22 Snowy Mouse\"
+        END
+    END
+    BLOCK \"VarFileInfo\"
+    BEGIN
+        VALUE \"Translation\", 0x409, 1200
+    END
+END
+
+IDI_ICON1 ICON DISCARDABLE \"icon/icon.ico\"
+").unwrap();
+
+        embed_resource::compile(windows_rc_path);
     }
 
     // Store gorilla version
-    println!("cargo:rustc-env=gorilla_version={} {}", std::env::var("CARGO_PKG_NAME").unwrap(), std::env::var("CARGO_PKG_VERSION").unwrap());
+    println!("cargo:rustc-env=gorilla_version={} {}", gorilla_pkg, gorilla_version);
 }
